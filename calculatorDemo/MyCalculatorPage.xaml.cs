927ca5bd37e7d19679efefcfbd522ae5748bde6b
AdityaRayamajhi <aditya.rayamajhi@gmail.com>
2021-09-22 22:23:38 -0600

    first commit

---
+++ calculatorDemo/MyCalculatorPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace calculatorDemo
+{
+    public partial class MyCalculatorPage : ContentPage
+    {
+        public string BttnText { get; set; }
+
+        public MyCalculatorPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MyCalculatorViewModel();
+        }
+
+        //void NavgAction(System.Object sender, System.EventArgs e)
+        //{
+        //    Navigation.PushAsync(new MainPage());
+        //}
+    }
+}
---
+++ calculatorDemo/MyCalculatorViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace calculatorDemo
+{
+    public class MyCalculatorViewModel : BaseViewModel
+    {
+        string _bttnText;
+        public string BttnText
+        {
+            get { return _bttnText; }
+            set { SetProperty(ref _bttnText, value); }
+        }
+
+        public ICommand NavgCommand
+        {
+            get { return new Command(HandleNavgAction); }
+        }
+
+        private void HandleNavgAction(object obj)
+        {
+            var a = "b";
+            ///Navigate from this place...
+            ///
+        }
+
+        public MyCalculatorViewModel()
+        {
+            BttnText = "View Model Bttn";
+
+
+        }
+
+    }
+}
